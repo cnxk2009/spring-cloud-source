f454678f613415a86552d0e2214cf36cb961c0d3
cnxk2009 <cnxk2009@163.com>
2020-01-12 21:13:13 +0800

    init eureka source

--- spring-cloud-eureka-source/src/main/java/org/soucecode/springboot2/EurekaServerApplication.java
+++ spring-cloud-eureka-source/src/main/java/org/soucecode/springboot2/EurekaServerApplication.java
@@ -5,19 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class SpringBoot2Application extends SpringBootServletInitializer {
+public class EurekaServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot2Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
-    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { return application.sources(SpringBoot2Application.class); }
+    @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { return application.sources(EurekaServerApplication.class); }
 
     /**
      * commandLineRunner方法被注解成了@Bean，程序在启动的时候，这个方法会被自动执行，
