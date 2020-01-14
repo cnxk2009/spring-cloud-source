c0859f39bfb5cdac878ddce6eb9b7bf97627dc16
cnxk2009 <cnxk2009@163.com>
2020-01-14 19:51:01 +0800

    引入Hystrix

--- spring-cloud-ribbon-feign-hystrix-source/src/main/java/org/soucecode/springboot2/RibbonFeignApplication.java
+++ spring-cloud-ribbon-feign-hystrix-source/src/main/java/org/soucecode/springboot2/RibbonFeignApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
+@EnableFeignClients
 @SpringBootApplication
 public class RibbonFeignApplication extends SpringBootServletInitializer {
 
