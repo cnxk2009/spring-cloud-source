56c4aa5de7831d8b742a6e553b3ce90ff9e162ce
cnxk2009 <cnxk2009@163.com>
2020-02-05 18:03:09 +0800

    增加Zuul源码及注解

--- spring-cloud-zuul-source/src/main/java/org/soucecode/springboot2/GateWayZuulApplication.java
+++ spring-cloud-zuul-source/src/main/java/org/soucecode/springboot2/GateWayZuulApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GateWayZuulApplication extends SpringBootServletInitializer {
 
@@ -14,4 +19,9 @@ public class GateWayZuulApplication extends SpringBootServletInitializer {
 
     @Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { return application.sources(GateWayZuulApplication.class); }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
